a39819c3462b3720af075b6159072f61838e4f13
stageplek unifact <stageplek@unifact.eu>
2015-06-19 09:00:04 +0200

    contact form
    Contact form werkt krijgen (nog niet gelukt)

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,8 +1,11 @@
 <?php
+session_start();
+
+require_once 'phpmailer/PHPMailerAutoload.php';
+
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
+if(empty($_POST['name'])  	||
+   empty($_POST['email']) 	||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -11,8 +14,7 @@ if(empty($_POST['name'])  		||
    }
 	
 $name = $_POST['name'];
-$email_address = $_POST['email'];
-$phone = $_POST['phone'];
+$email_adres = $_POST['email'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
@@ -20,7 +22,8 @@ $to = 'wilcologger@gmail.com'; // Add your email address inbetween the '' replac
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$headers .= "Reply-To: $email_adres";
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+return true;
+
 ?>
